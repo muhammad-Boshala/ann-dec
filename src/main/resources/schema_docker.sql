4010eb0342cd947b3a1e6307af1a2d8813b24ce2
ayuob <ayuob.it@gmail.com>
2023-05-31 00:54:11 +0200

    get id back becuse SSN should be changeable and no mutable and id should be mutable

--- src/main/resources/schema_docker.sql
+++ src/main/resources/schema_docker.sql
@@ -2,42 +2,49 @@
 CREATE SCHEMA pensioners;
 
 -- Create the SocialSecurityPensioner table under the pensioner schema
-CREATE TABLE pensioners.pensioner (
-                                                     national_number VARCHAR(12) PRIMARY KEY,
-                                                     pension_number VARCHAR(10) NOT NULL,
-                                                     name VARCHAR(100) NOT NULL,
-                                                     date_of_birth DATE NOT NULL,
-                                                     address VARCHAR(200) NOT NULL
+CREATE TABLE pensioners.pensioner(
+                                     pensioner_id SERIAL PRIMARY KEY,
+                                     national_number VARCHAR(12) NOT NULL,
+                                     pension_number VARCHAR(10) NOT NULL,
+                                     name VARCHAR(100) NOT NULL,
+                                     date_of_birth DATE NOT NULL,
+                                     address VARCHAR(200) NOT NULL,
+                                    CONSTRAINT unique_national_number UNIQUE (national_number)
+
 );
 
 -- Create the FamilyMember table under the pensioner schema
-CREATE TABLE pensioners.family_member (
-                                         national_number VARCHAR(12) PRIMARY KEY,
+CREATE TABLE pensioners.family_member(
+                                         family_member_id SERIAL PRIMARY KEY,
+                                         pensioner_id INT NOT NULL,
+                                         national_number VARCHAR(12) NOT NULL,
                                          name VARCHAR(100) NOT NULL,
                                          date_of_birth DATE NOT NULL,
                                          gender VARCHAR(10) NOT NULL,
-                                         FOREIGN KEY (national_number) REFERENCES pensioners.pensioner(national_number)
+                                         FOREIGN KEY (pensioner_id) REFERENCES pensioners.pensioner(pensioner_id),
+                                         CONSTRAINT unique_family_member_national_number UNIQUE (national_number)
+
 );
 
 -- Create the Beneficiary table under the pensioner schema
-CREATE TABLE pensioners.beneficiary (
-                                       family_member_national_number VARCHAR(12) PRIMARY KEY,
+CREATE TABLE pensioners.beneficiary(
+                                       family_member_id INT PRIMARY KEY,
                                        entitlement_type VARCHAR(100) NOT NULL,
                                        entitlement_details TEXT,
-                                       FOREIGN KEY (family_member_national_number) REFERENCES pensioners.family_member(national_number)
+                                       FOREIGN KEY (family_member_id) REFERENCES pensioners.family_member(family_member_id)
 );
 
 -- Create the AnnualDeclaration table under the pensioner schema
-CREATE TABLE pensioners.annual_declaration (
+CREATE TABLE pensioners.annual_declaration(
                                               declaration_id SERIAL PRIMARY KEY,
-                                              pensioner_national_number VARCHAR(12) NOT NULL,
+                                              pensioner_id INT NOT NULL,
                                               submission_date DATE NOT NULL,
                                               status VARCHAR(20) NOT NULL,
-                                              FOREIGN KEY (pensioner_national_number) REFERENCES pensioners.pensioner(national_number)
+                                              FOREIGN KEY (pensioner_id) REFERENCES pensioners.pensioner(pensioner_id)
 );
 
 -- Create the Document table under the pensioner schema
-CREATE TABLE pensioners.document (
+CREATE TABLE pensioners.document(
                                     document_id SERIAL PRIMARY KEY,
                                     declaration_id INT NOT NULL,
                                     file_name VARCHAR(200) NOT NULL,
@@ -46,7 +53,7 @@ CREATE TABLE pensioners.document (
 );
 
 -- Create the Administrator table under the pensioner schema
-CREATE TABLE pensioners.administrator (
+CREATE TABLE pensioners.administrator(
                                          administrator_id SERIAL PRIMARY KEY,
                                          username VARCHAR(100) NOT NULL,
                                          password VARCHAR(100) NOT NULL
