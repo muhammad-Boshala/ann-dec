379388e85f19fbb78f0d6b4e00a352485b6c049c
ayuob <ayuob.it@gmail.com>
2023-05-30 18:14:18 +0200

    rename pensioner Schema to pensioners, plural is more meaningful

--- src/main/resources/schema_docker.sql
+++ src/main/resources/schema_docker.sql
@@ -1,8 +1,8 @@
 -- Create the schema
-CREATE SCHEMA pensioner;
+CREATE SCHEMA pensioners;
 
 -- Create the SocialSecurityPensioner table under the pensioner schema
-CREATE TABLE pensioner.pensioner (
+CREATE TABLE pensioners.pensioner (
                                                      national_number VARCHAR(12) PRIMARY KEY,
                                                      pension_number VARCHAR(10) NOT NULL,
                                                      name VARCHAR(100) NOT NULL,
@@ -11,55 +11,55 @@ CREATE TABLE pensioner.pensioner (
 );
 
 -- Create the FamilyMember table under the pensioner schema
-CREATE TABLE pensioner.family_member (
+CREATE TABLE pensioners.family_member (
                                          national_number VARCHAR(12) PRIMARY KEY,
                                          name VARCHAR(100) NOT NULL,
                                          date_of_birth DATE NOT NULL,
                                          gender VARCHAR(10) NOT NULL,
-                                         FOREIGN KEY (national_number) REFERENCES pensioner.pensioner(national_number)
+                                         FOREIGN KEY (national_number) REFERENCES pensioners.pensioner(national_number)
 );
 
 -- Create the Beneficiary table under the pensioner schema
-CREATE TABLE pensioner.beneficiary (
+CREATE TABLE pensioners.beneficiary (
                                        family_member_national_number VARCHAR(12) PRIMARY KEY,
                                        entitlement_type VARCHAR(100) NOT NULL,
                                        entitlement_details TEXT,
-                                       FOREIGN KEY (family_member_national_number) REFERENCES pensioner.family_member(national_number)
+                                       FOREIGN KEY (family_member_national_number) REFERENCES pensioners.family_member(national_number)
 );
 
 -- Create the AnnualDeclaration table under the pensioner schema
-CREATE TABLE pensioner.annual_declaration (
+CREATE TABLE pensioners.annual_declaration (
                                               declaration_id SERIAL PRIMARY KEY,
                                               pensioner_national_number VARCHAR(12) NOT NULL,
                                               submission_date DATE NOT NULL,
                                               status VARCHAR(20) NOT NULL,
-                                              FOREIGN KEY (pensioner_national_number) REFERENCES pensioner.pensioner(national_number)
+                                              FOREIGN KEY (pensioner_national_number) REFERENCES pensioners.pensioner(national_number)
 );
 
 -- Create the Document table under the pensioner schema
-CREATE TABLE pensioner.document (
+CREATE TABLE pensioners.document (
                                     document_id SERIAL PRIMARY KEY,
                                     declaration_id INT NOT NULL,
                                     file_name VARCHAR(200) NOT NULL,
                                     file_path VARCHAR(200) NOT NULL,
-                                    FOREIGN KEY (declaration_id) REFERENCES pensioner.annual_declaration(declaration_id)
+                                    FOREIGN KEY (declaration_id) REFERENCES pensioners.annual_declaration(declaration_id)
 );
 
 -- Create the Administrator table under the pensioner schema
-CREATE TABLE pensioner.administrator (
+CREATE TABLE pensioners.administrator (
                                          administrator_id SERIAL PRIMARY KEY,
                                          username VARCHAR(100) NOT NULL,
                                          password VARCHAR(100) NOT NULL
 );
 
 -- Grant privileges to the user on the pensioner schema
-GRANT ALL PRIVILEGES ON SCHEMA pensioner TO ad_user;
+GRANT ALL PRIVILEGES ON SCHEMA pensioners TO ad_user;
 
 -- Grant privileges to the user on the tables within the pensioner schema
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA pensioner TO ad_user;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA pensioners TO ad_user;
 
 -- Grant privileges to the user on sequences within the pensioner schema
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA pensioner TO ad_user;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA pensioners TO ad_user;
 
 -- Grant privileges to the user on functions within the pensioner schema
-GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA pensioner TO ad_user;
+GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA pensioners TO ad_user;
